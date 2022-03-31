42dfac54a59885677cf856bfa3225f428429081a
huongbtph16610 <buithihuong20080@gmail.com>
2022-03-31 19:58:12 +0700

    update

--- routers/product.js
+++ routers/product.js
@@ -4,9 +4,9 @@ import { create, list, read, remove, update } from "../controllers/product";
 const router = Router();
 
 router.get('/products', list);
-router.get('/product/:id', read);
+router.get('/products/:id', read);
 router.post('/products', create);
-router.delete('/product/:id', remove);
-router.patch('/product/:id', update);
+router.delete('/products/:id', remove);
+router.patch('/products/:id', update);
 
 export default router;
